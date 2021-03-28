242b090a2c787bd12ac1b719a010edeb4e41d82c
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-28 22:29:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Connects the buttons to their functions.
+document.getElementById('submit-area').addEventListener('click', calcArea)
+document.getElementById('submit-circumference').addEventListener('click', circ)
+
+let diameter = 0
+let circleArea = 0
+let circleCirc = 0
+let answerArea = 0
+let answerCirc = 0
+let pi = 3.14159
+
+function calcArea () {
+  diameter = document.getElementById('area').value
+  if (diameter < 0) {
+    alert('Do not use negative numbers.')
+  }
+  answerArea = pi * ((diameter / 2) * (diameter / 2))
+  document.getElementById('answer').innerHTML = answerArea
+}
+
+function circ () {
+  diameter = document.getElementById('circumference').value
+  if (diameter < 0) {
+    alert('Do not use negative numbers.')
+  }
+  answerCirc = (pi * diameter)
+  document.getElementById('answer2').innerHTML = answerCirc
+}
